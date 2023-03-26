93edf0a969549f6e1d9e2d392027f8e07ff1ecf6
FlorentinaIonescu <ionescoflorin@gmail.com>
2023-03-26 16:37:50 +0300

    13/100

--- ProjectEuler/Problem12.cs
+++ ProjectEuler/Problem12.cs
@@ -10,6 +10,42 @@ namespace ProjectEuler
 {
     public static class Problem12
     {
-        // 
+        // ...What is the value of the first triangle number to have over five hundred divisors?
+
+        static void Main(string[] args)
+        {
+            int n = 1;
+            int triangularNumber = 0;
+            int numDivisors = 0;
+
+            while (numDivisors <= 500)
+            {
+                triangularNumber += n;
+                numDivisors = GetNumDivisors(triangularNumber);
+                n++;
+            }
+
+            Console.WriteLine("The first triangular number with over 500 divisors is: " + triangularNumber);
+        }
+
+        static int GetNumDivisors(int n)
+        {
+            int numDivisors = 0;
+
+            for (int i = 1; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    numDivisors += 2;
+                }
+            }
+
+            if (Math.Sqrt(n) * Math.Sqrt(n) == n)
+            {
+                numDivisors--;
+            }
+
+            return numDivisors;
+        }
     }
 }

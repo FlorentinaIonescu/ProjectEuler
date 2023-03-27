f36367a0c878bcb284feccf36f16832127e4e611
FlorentinaIonescu <ionescoflorin@gmail.com>
2023-03-27 20:38:38 +0300

    15/100

--- ProjectEuler/Problem14.cs
+++ ProjectEuler/Problem14.cs
@@ -10,6 +10,40 @@ namespace ProjectEuler
 {
     public static class Problem14
     {
-        // 
+        // ... Which starting number, under one million, produces the longest chain?
+
+        static void Main(string[] args)
+        {
+            long maxLength = 0;
+            long maxStartingNumber = 0;
+
+            for (long i = 1; i < 1000000; i++)
+            {
+                long n = i;
+                long length = 1;
+
+                while (n != 1)
+                {
+                    if (n % 2 == 0)
+                    {
+                        n = n / 2;
+                    }
+                    else
+                    {
+                        n = 3 * n + 1;
+                    }
+                    length++;
+                }
+
+                if (length > maxLength)
+                {
+                    maxLength = length;
+                    maxStartingNumber = i;
+                }
+            }
+
+            Console.WriteLine("The starting number {0} produces a sequence of {1} terms.", maxStartingNumber, maxLength);
+            Console.ReadKey();
+        }
     }
 }
---
+++ ProjectEuler/Problem15.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public static class Problem15
+    {
+        // Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+        // How many such routes are there through a 20×20 grid?
+
+        static void Main(string[] args)
+        {
+            const int gridSize = 20;
+            long[,] grid = new long[gridSize + 1, gridSize + 1];
+
+            for (int i = 0; i <= gridSize; i++)
+            {
+                for (int j = 0; j <= gridSize; j++)
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        grid[i, j] = 1;
+                    }
+                    else
+                    {
+                        grid[i, j] = grid[i - 1, j] + grid[i, j - 1];
+                    }
+                }
+            }
+
+            Console.WriteLine("Number of routes in a {0}x{0} grid: {1}", gridSize, grid[gridSize, gridSize]);
+        }
+    }
+}
